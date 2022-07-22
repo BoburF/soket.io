4c5fce1b05a4b2b60b6c3a21d7f87a8184f98be4
baxromxoja16 <changeitgo3@gmail.com>
2022-07-22 13:03:59 +0500

    fix: port

--- index.js
+++ index.js
@@ -68,6 +68,6 @@ io.on('connection', (socket) => {
 
 })
 
-const PORT = 3000 || process.env.PORT
+const PORT = process.env.PORT || 3000
 
-server.listen(3000, () => console.log(`Server working on port ${PORT}`))
+server.listen(PORT, () => console.log(`Server working on port ${PORT}`))
